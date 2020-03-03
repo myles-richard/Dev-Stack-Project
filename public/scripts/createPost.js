54f807c3fc5b9bb7434fc07e06604b5061c756df
myles-richard <myrich22@gmail.com>
2020-03-03 14:29:30 -0800

    got add post working

--- public/scripts/createPost.js
+++ public/scripts/createPost.js
@@ -5,8 +5,10 @@ document.getElementById('postBtn').addEventListener('click', createPost);
 
 function createPost(e){
     e.preventDefault()
-//     // const title = document.querySelector('#name').value
-//     // console.log(title);
+    const title = document.querySelector('#fname').value
+    const body = document.querySelector('#lastName').value
+    const name = document.querySelector('#name')
+    const newPost = {title, body, name};
     
 //     // const description = document.querySelector('#description').value
 //     // console.log(description);
@@ -30,40 +32,38 @@ function createPost(e){
 // //     .then(stream => stream.json())
 // //     .then(res => render(res))
 // // }
-fetch('https://jsonplaceholder.typicode.com/posts')
-  .then(response => response.json())
-  .then(json => console.log(json))
+fetch('/api/posts', {
+    method: 'POST',
+    headers: {
+        'Content-Type': 'application/json',
+    },
+    body: JSON.stringify(newPost)
+})
+  .then(stream => stream.json())
+  .then(res => render(res))
 }
+
 function render(posts){
     console.log(posts)
-    // posts.forEach(post => {
-    //     const postTemplate = getPostTemplate(post);
-    //     postsContainer.insertAdjacentHTML('beforeend', postTemplate);
-    // })
-    // postsContainer.insertAdjacentHTML('afterbegin', getPostTemplate(post))
+    
+    const postTemplate = getPostTemplate(posts);
+    postsContainer.insertAdjacentHTML('beforeend', postTemplate)
     // console.log(post)
 }
 
 function getPostTemplate(post) {
     return `
-    <div class="post">
-    <p>${post.title}</p>
-    <p>${post.body}</p>
-    </div>
+    <div class="col-md-3 pb-md-3 mt-sm-3 "> 
+            <div class="card" style=" width: 16rem;">
+                <div class="card-body border border-warning shadow rounded bg-dark">
+                <h5 class="card-title text-warning">${post.title}</h5>
+                <h6 class="card-subtitle mb-4 text-light">${post.name}</h6>
+                <p class="card-text text-warning">${post.body}</p>
+                <a href="#" class="card-link">save</a>
+                <button type="button" class="btn btn-outline-warning float-right">save</button>
+                </div>
+            </div>
+          </div>
+    
     `;
 }
-// function getPostTemplate(post){
-//     return `
-//     <div class="col-md-3 pb-md-3 mt-sm-3 "> 
-//                 <div class="card" style=" width: 16rem;">
-//                     <div class="card-body border border-warning shadow rounded bg-dark">
-//                     <h5 class="card-title text-warning">${title}</h5>
-//                     <h6 class="card-subtitle mb-4 text-light">Sonia</h6>
-//                     <p class="card-text text-warning">${description}</p>
-//                     <a href="#" class="card-link">save</a>
-//                     <button type="button" class="btn btn-outline-warning float-right">save</button>
-//                     </div>
-//                 </div>
-//             </div>
-//     `
-// }
